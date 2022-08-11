ddc5c230c556d5cd316c1d6f38cc2f9183a61123
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-08-11 20:38:07 +0700

    Menambahkan struct untuk reqeuest create user dan update user pada package usersdto serta memindahkan file result.go ke dalam folder dto/result

---
+++ dto/users/user_request.go
@@ -0,0 +1,13 @@
+package usersdto
+
+type CreateUserRequest struct {
+	FullName string `json:"fullName" form:"name" validate:"required"`
+	Email    string `json:"email" form:"email" validate:"required"`
+	Password string `json:"password" form:"password" validate:"required"`
+}
+
+type UpdateUserRequest struct {
+	FullName string `json:"fullName" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
