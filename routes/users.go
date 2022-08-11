dfc675b4acb6b568ca1d5791673afdeefa9c8c7a
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-08-11 21:51:46 +0700

    Menambahkan User Routes

---
+++ routes/users.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"waysbuck/handlers"
+	"waysbuck/pkg/mysql"
+	"waysbuck/repositories"
+
+	"github.com/gorilla/mux"
+)
+
+func UserRoutes(r *mux.Router) {
+	userRepository := repositories.RepositoryUser(mysql.DB)
+	h := handlers.HandlerUser(userRepository)
+
+	r.HandleFunc("/users", h.FindUsers).Methods("GET")
+}
