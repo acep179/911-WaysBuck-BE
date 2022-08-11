7aff37301f087c0fd220cdac18b29c091d1ec1e1
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-08-11 21:17:30 +0700

    Membuat Repository untuk User serta membuat function FindUsers pada repository untuk mendapatkan seluruh data user

---
+++ repositories/users.go
@@ -0,0 +1,26 @@
+package repositories
+
+import (
+	"waysbuck/models"
+
+	"gorm.io/gorm"
+)
+
+type UserRepository interface {
+	FindUsers() ([]models.User, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func RepositoryUser(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+func (r *repository) FindUsers() ([]models.User, error) {
+	var users []models.User
+	err := r.db.Raw("SELECT * FROM users").Scan(&users).Error
+
+	return users, err
+}
