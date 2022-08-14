5f42b29b093a15a380df2ca0f001a1bb92edf416
Acep Awaludin <acep.awal.sosmed@gmail.com>
2022-08-14 15:11:22 +0700

    Menambahkan LoginResponse pada authdto

--- dto/auth/auth_response.go
+++ dto/auth/auth_response.go
@@ -6,3 +6,10 @@ type RegisterResponse struct {
 	Email    string `json:"email"  form:"email" validate:"required"`
 	Password string `json:"password"  form:"password" validate:"required"`
 }
+
+type LoginResponse struct {
+	FullName string `gorm:"type: varchar(255)" json:"fullName"`
+	Email    string `gorm:"type: varchar(255)" json:"email"`
+	Password string `gorm:"type: varchar(255)" json:"password"`
+	Token    string `gorm:"type: varchar(255)" json:"token"`
+}
