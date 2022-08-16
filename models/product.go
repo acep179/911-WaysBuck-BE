487dd912979c6dd0913d9fc01b1be1f067267349
unknown <finaralpf@gmail.com>
2022-08-16 22:59:17 +0700

    relasi antar table

--- models/product.go
+++ models/product.go
@@ -5,7 +5,7 @@ import "time"
 type Product struct {
 	ID        int            `json:"id" gorm:"primary_key:auto_increment"`
 	Title     string         `json:"title" gorm:"type: varchar(255)"`
-	Price     int            `json:"price" gorm:"type: varchar(255)"`
+	Price     int            `json:"price" gorm:"type: int"`
 	Image     string         `json:"image" gorm:"type: varchar(255)"`
 	UserID    int            `json:"-"`
 	User      UserProfileRel `json:"user"`
@@ -13,14 +13,22 @@ type Product struct {
 	UpdatedAt time.Time      `json:"-"`
 }
 
-type ProductUserRel struct {
-	ID     int    `json:"id" gorm:"primary_key:auto_increment"`
-	Title  string `json:"title" gorm:"type: varchar(255)"`
-	Price  int    `json:"price" gorm:"type: varchar(255)"`
-	Image  string `json:"image" gorm:"type: varchar(255)"`
-	UserID int    `json:"-"`
+type ProductCartRel struct {
+	ID    int    `json:"id"`
+	Title string `json:"title" gorm:"type: varchar(255)"`
+	Price int    `json:"price" gorm:"type: int"`
+	Image string `json:"image" gorm:"type: varchar(255)"`
 }
 
+type ProductUserRel struct {
+	Title string `json:"title" gorm:"type: varchar(255)"`
+	Price int    `json:"price" gorm:"type: int"`
+	Image string `json:"image" gorm:"type: varchar(255)"`
+}
+
+func (ProductCartRel) TableName() string {
+	return "products"
+}
 func (ProductUserRel) TableName() string {
 	return "products"
 }
--- models/user.go
+++ models/user.go
@@ -5,23 +5,23 @@ import (
 )
 
 type User struct {
-	ID        int              `json:"id"`
-	FullName  string           `json:"fullName" gorm:"type: varchar(255)"`
-	Email     string           `json:"email" gorm:"type: varchar(255)"`
-	Password  string           `json:"password" gorm:"type: varchar(255)"`
-	Status    string           `json:"status"`
-	Profile   ProfileResponse  `json:"profile"`
-	Products  []ProductUserRel `json:"products"`
-	CreatedAt time.Time        `json:"-"`
-	UpdatedAt time.Time        `json:"-"`
+	ID          int                  `json:"id"`
+	FullName    string               `json:"fullName" gorm:"type: varchar(255)"`
+	Email       string               `json:"email" gorm:"type: varchar(255)"`
+	Password    string               `json:"password" gorm:"type: varchar(255)"`
+	Status      string               `json:"status"`
+	Profile     ProfileResponse      `json:"profile"`
+	Transaction []TransactionUserRel `json:"transaction"`
+	CreatedAt   time.Time            `json:"-"`
+	UpdatedAt   time.Time            `json:"-"`
 }
 
 // relation to the another table
 // atribut yang di tulis harus sama
-
 type UserProfileRel struct {
 	ID       int    `json:"id"`
 	FullName string `json:"fullName"`
+	Email    string `json:"email"`
 }
 
 func (UserProfileRel) TableName() string {
